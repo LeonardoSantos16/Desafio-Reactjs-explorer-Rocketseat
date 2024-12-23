5810a75f3b81b8744f89c3d41efc322ae6b2c160
leonardo.mendes <leodsmf@gmail.com>
2024-12-22 22:14:22 -0300

    withCredentials

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'https://desafio-node-movie-rocketseat.onrender.com'
+    baseURL: 'https://desafio-node-movie-rocketseat.onrender.com',
+    withCredentials: true
 })
